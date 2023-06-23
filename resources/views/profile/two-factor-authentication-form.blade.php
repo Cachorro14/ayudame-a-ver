e75bc5bc2f7a09100e82d8c2d5fb97fcbc4a0e58
Cachorro14 <carlos.duenas6017@alumnos.udg.mx>
2023-06-23 16:39:23 -0600

    Cambios a español

--- resources/views/profile/two-factor-authentication-form.blade.php
+++ resources/views/profile/two-factor-authentication-form.blade.php
@@ -1,6 +1,6 @@
 <x-jet-action-section>
     <x-slot name="title">
-        {{ __('Two Factor Authentication') }}
+        {{ __('Autenticacion de') }}
     </x-slot>
 
     <x-slot name="description">
