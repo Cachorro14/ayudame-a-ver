be37459f5550c17d5647f08df3ac7274f49edc5a
Cachorro14 <carlos.duenas6017@alumnos.udg.mx>
2023-06-29 20:57:40 -0600

    mejoras visuales

--- resources/views/layouts/landing-page.blade.php
+++ resources/views/layouts/landing-page.blade.php
@@ -67,7 +67,7 @@
         </nav>
     </header>
 
-    <section class="bg-slate-200 dark:bg-gray-900 sm:mt-9" id="section">
+    <section class="bg-slate-200 dark:bg-gray-900 mt-14 lg:mt-0" id="section">
         <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
             <div class="mr-auto place-self-center lg:col-span-7">
                 <h1 class="max-w-2xl mb-4 text-4xl font-extrabold leading-none md:text-5xl xl:text-6xl dark:text-white">
